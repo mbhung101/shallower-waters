15780abcd69f2fbfe88bea7ecbb42189fe53e5b7
mbhung101 <github email address>
2020-07-14 23:27:16 -0400

    added files

---
+++ src/containers/home.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { Menu } from 'semantic-ui-react'
+import MenuTop from '../components/menutop'
+
+class Home extends Component {
+
+  constructor(props) {
+  super(props);
+  this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Home
